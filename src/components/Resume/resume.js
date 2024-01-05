8d789407214f468acbddf21b1267ba91d3f3fd82
Zachariah Dellimore <zachariahdellimore03@gmail.com>
2024-01-04 20:09:38 -0500

    Updated Resume

--- src/components/Resume/resume.js
+++ src/components/Resume/resume.js
@@ -9,8 +9,8 @@ const Resume = () => {
       <div className='resumeDesc'>
         <p>My resume showcases a culmination of my journey—highlighting my dedication C++ and Desktop Applications. I invite you to explore my personal and professional accomplishments and discover how my expertise can contribute to your next project or team.</p>
       </div>
-      <iframe src="https://docs.google.com/viewer?srcid=1bI-KAz7zMgD96oSYn-TmFxWq47ts33ya&pid=explorer&efh=false&a=v&chrome=false&embedded=true" title='My Resume' className='resumeFrame' alt='My Resume'/>
-    </section>
+      <iframe src="https://docs.google.com/viewer?srcid=17nWEnTPHwTUzDpHxB7WFbo4B-ExhF-9S&pid=explorer&efh=false&a=v&chrome=false&embedded=true" title='My Resume' className='resumeFrame' alt='My Resume'/>
+    </section> 
   )
 }
 
